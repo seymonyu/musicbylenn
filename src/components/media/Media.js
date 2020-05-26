366a0f2efc99434ee03ffcf377a3aa718fe36dd3
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-05-27 00:33:03 +0200

    photo grid was made

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -2,7 +2,38 @@ import React from "react";
 import "./media.scss";
 
 function Media() {
-  return <div>medisa</div>;
+  const images = [
+    "https://cdn.pixabay.com/photo/2020/05/24/02/00/barber-shop-5212059_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/20/06/47/mountain-5195052_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/21/13/33/blue-flax-5200811_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/24/02/00/barber-shop-5212059_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/20/06/47/mountain-5195052_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/21/13/33/blue-flax-5200811_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/24/02/00/barber-shop-5212059_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/20/06/47/mountain-5195052_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/21/13/33/blue-flax-5200811_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/24/02/00/barber-shop-5212059_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/20/06/47/mountain-5195052_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/21/13/33/blue-flax-5200811_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/24/02/00/barber-shop-5212059_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/20/06/47/mountain-5195052_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2020/05/21/13/33/blue-flax-5200811_960_720.jpg",
+  ];
+
+  return (
+    <div className="media--wrapper">
+      <div className="media--gallery">
+        {images.map((item, index) => (
+          <img
+            className="media--gallery_img "
+            key={index}
+            src={item}
+            alt="index"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Media;
--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./music.scss";
 
 function Music() {
-  return <div className="music--wrapper">music</div>;
+  return (
+    <div className="music--wrapper">
+      <div className="music--spotify">
+        <a
+          href="https://open.spotify.com/artist/6D53Kee5eQV6bW3TBNWIzE"
+          target="_blank"
+        >
+          <img
+            className="music--img"
+            src="https://i.scdn.co/image/90d8d3ea4b10c8375ca60c6cca52f6c9541b03fe"
+          ></img>
+        </a>
+        <div className="bd">
+          <button className="music--button" title="Spotify'da takip et">
+            <span className="bt-icon"></span>
+            <span className="bt-text">Takip Et</span>
+          </button>
+          <div class="count" data-followers="163">
+            <div class="count-num front">163</div>
+            <div class="count-num back">+1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Music;
