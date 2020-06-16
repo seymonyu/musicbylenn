6b7387c893e29b35927a645fbe79392825d90fd2
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-06-16 15:55:54 +0200

    logo fixed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.scss";
 import Sound from "../sound/Sound";
+import whitelogo from "../../logo/whitelogo.png"
 
 function Home() {
   return (
@@ -10,12 +11,12 @@ function Home() {
       <div className="home--logo">
         <img
           className="home--logo_img"
-          src="https://static.wixstatic.com/media/e5e60c_cc0de99490764ccca7848c6ba9eb8ad6~mv2.png/v1/fill/w_853,h_119,al_c,q_85,usm_0.66_1.00_0.01/LOGO%20LENN%202019%20wit.webp"
+          src={whitelogo}
           alt="lenn-logo"
         />
          <img
           className="home--logo_img_mobile"
-          src="https://static.wixstatic.com/media/e5e60c_cc0de99490764ccca7848c6ba9eb8ad6~mv2.png/v1/fill/w_853,h_119,al_c,q_85,usm_0.66_1.00_0.01/LOGO%20LENN%202019%20wit.webp"
+          src={whitelogo}
           alt="lenn-logo-mobile"
         />
       </div>
