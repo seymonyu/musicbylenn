2d6834967a5d42bbe342b752f8f3486810502723
seymonyu <59199817+seymonyu@users.noreply.github.com>
2020-06-19 14:53:22 +0200

    spotify

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -4,8 +4,7 @@ import "./music.scss";
 function Music() {
   return (
     <div className="music--wrapper">
-    <iframe title ='soundcloud'width="80%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/117203320&color=%232af598&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&mute=true"></iframe><div ><a href="https://soundcloud.com/musicbylenn" title="LENN" rel="noopener noreferrer" target="_blank" >LENN</a></div>
-     {/* <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/117203320&color=%232af598&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe> */}
+    <iframe title ='spotify' src="https://open.spotify.com/embed/playlist/37H7j4nyqxGu0KEBDuShJ8" width="1100" height="500" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
   );
  }
