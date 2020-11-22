2daa0de0f716b27e8acb229feb6a1a98f092aaaa
seymonyu <seymafirat@outlook.com>
2020-11-22 15:41:55 +0100

    lenn page

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -4,7 +4,7 @@ import "./music.scss";
 function Music() {
   return (
     <div className="music--wrapper">
-      <iframe src="https://open.spotify.com/embed/artist/6D53Kee5eQV6bW3TBNWIzE" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe src="https://open.spotify.com/embed/artist/6D53Kee5eQV6bW3TBNWIzE" width="300" height="380" frameborder="0.5" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
   );
  }
